76d7f5f6d420d78939550cdfcbd3a5a269a4d680
Andrew Cox <aedryan@gmail.com>
2017-04-19 14:01:34 -0500

    Finished adding functionality.

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+const multer  = require('multer');
 const path = require("path");
 const express = require('express');
 const app = express();
+const upload = multer({ dest: '' });
 
 app.set('port', (process.env.PORT || 5000));
 app.use('/favicon.ico', express.static("favicon.ico"));
+app.use('/app.js', express.static("app.js"));
+
+app.post('/file-size', upload.single('newFile'), (req, res) => {
+   res.json({ size: req.file.size });
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
